7dfbdb5e9b4ac99e3815c1ed9772191ceaa87df1
lainepos <lainepos@gmail.com>
2021-08-12 20:17:10 -0300

    Atualização

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -5,11 +5,14 @@ package petstore;
 //2- Bibliotecas
 
 
+import io.restassured.specification.Argument;
+import org.hamcrest.Matcher;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,32 +21,34 @@ public class Pet {
     //3.1 Atribuitos
     //URI - endereço da documentação
     String uri = "https://petstore.swagger.io/v2/pet";
+    private Object available;
 
     //3.2 métodos e funções
-       public String lerJson(String camihoJson) throws IOException {
+    public String lerJson(String camihoJson) throws IOException {
 
-           return new String(Files.readAllBytes(Paths.get(camihoJson)));
-       }
+        return new String(Files.readAllBytes(Paths.get(camihoJson)));
+    }
 
-        // incluir um novo registro
-        @Test    // Identifica o métido ou função como teste para o testNG
+    // incluir um novo registro
+    @Test    // Identifica o métido ou função como teste para o testNG
 
-        public void incluirPet() throws IOException {
-           String jsonBody= lerJson("db/pet1.json");
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
 
-            // Sintaxe Gherkin
-            // Dado - Quando - Então
-            // Given - When - Then
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
 
-            given()
-                    .contentType("application/json") //comum em API Rest //API antiga é "text/xml"
-                    .log().all()
-                    .body(jsonBody)
-            .when()
-                    .post(uri)
-            .then()
-                    .log().all()
-                    .statusCode(200);
+        given()
+                .contentType("application/json") //comum em API Rest //API antiga é "text/xml"
+                .log().all()
+                .body(jsonBody)
+                .when()
+                .post(uri)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Billy Francisco"));
+    }
 
-        }
-}
+
